8fb6e1d12263d043df781b237f26f110a909bb2f
pavaniankam <pavani.ankam@jukshio.com>
2024-05-07 11:38:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pavani
+# pavani
---
+++ iterate.py
@@ -0,0 +1,6 @@
+num = list(range(10))
+previousNum = 0
+for i in num:
+    sum = previousNum + i
+    print('Current Number '+ str(i) + 'Previous Number ' + str(previousNum) + 'is ' + str(sum))
+    previousNum=i
